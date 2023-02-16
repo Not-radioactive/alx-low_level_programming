8e95c9dcf9d93bcb8a774f7abed06225a0383a74
Not-radioactive <activity.anas@gmail.com>
2023-02-16 11:39:35 -0800

    dsd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/**
+ * I know it dosent need a comment cuz its simple enough but I have to do it for betty 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
