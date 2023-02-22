5e44291d7d409be3ee337e23f4b91b0b7a17f705
Not-radioactive <activity.anas@gmail.com>
2023-02-22 03:30:08 -0800

    sda

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Description: print all alphabet in lowercase
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+	
+	for ( letter = 'a'; letter <= 'z'; letter++ )
+	{
+		_putchar(letter);
+	}
+	_putchar ('\n');
+}
