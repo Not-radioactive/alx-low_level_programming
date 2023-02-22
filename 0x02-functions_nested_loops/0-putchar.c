fb65d7219b0e2c93326fff387d1fb0269a7a0e93
Not-radioactive <activity.anas@gmail.com>
2023-02-22 03:17:31 -0800

    fds

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - write that fucking sentence
- * Return 0
+ * main - Entry point
+ * Description:  prints _putchar
+ * Return: 0
  */
+
+
 int main(void)
 {
-	printf("_putchar");
+	/* call instruction  putchar from main.h */
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
